6f12ba87f7bb409186876c207d83ff582a972014
tgd2 <59289427+tgd2@users.noreply.github.com>
2020-10-18 22:54:31 +0100

    Streamlined the screen drawing routine
    Streamlined the screen drawing routine, and some other minor tweaks
    Now down to less than 100 lines of code (+ olc6502 and PGE)

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
-#include <sstream>
-
-#include "Bus.h"
-#include "olc6502.h"
-#include "ram_dump.h"
-
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+#include "olc6502.h"
+#include "Bus.h"
+#include "ram_dump.h"
+
 float fTimeSinceLastFrame = 0;
-bool bAtStartOfGameLoop = false;
 
 class Exile_olc6502 : public olc::PixelGameEngine
 {
@@ -20,20 +16,15 @@ public:
 
 	bool OnUserCreate()
 	{
-		// Load Exile RAM dump
+		// Load Exile RAM dump, and restore cpu state
 		for (long i = 0; i < 65536; i++) {
 			BBC.ram[i] = (uint8_t)Exile[i];
 		}
+		BBC.cpu.a = 0x01;		BBC.cpu.x = 0xff;		BBC.cpu.y = 0x51;
+		BBC.cpu.stkp = 0xfb;	BBC.cpu.pc = 0x1f63;
 
 		//Acquire all keys and equipment :)
 		for (long i = 0; i < (0x0818 - 0x0806); i++) BBC.ram[0x0806 + i] = 0xFF;
-
-		BBC.cpu.a = 0x01;
-		BBC.cpu.x = 0xff;
-		BBC.cpu.y = 0x51;
-
-		BBC.cpu.stkp = 0xfb;
-		BBC.cpu.pc = 0x1f63;
 
 		return true;
 	}
@@ -41,15 +32,12 @@ public:
 	bool OnUserUpdate(float fElapsedTime)
 	{
 		if ((fTimeSinceLastFrame += fElapsedTime) < 0.025) return true;
-		else fTimeSinceLastFrame = 0;
+		else fTimeSinceLastFrame = 0.0;
 
 		do {
 			do BBC.cpu.clock();
 			while (!BBC.cpu.complete());
-			bAtStartOfGameLoop = (BBC.cpu.pc == 0x19b6);
-		} while (!bAtStartOfGameLoop);
-
-		bAtStartOfGameLoop = false;
+		} while (BBC.cpu.pc != 0x19b6); //0x19b6 = Start of game loop
 
 		olc::Key Keys[39] = { olc::ESCAPE, olc::F10, olc::F1, olc::F2, olc::F3, olc::F4, olc::F5, olc::F6, olc::F7, olc::F8, olc::F9, olc::F9,
 							  olc::Key::G, olc::SPACE, olc::Key::I, olc::LEFT, olc::RIGHT, olc::UP, olc::DOWN, olc::Key::K, olc::Key::O, olc::Key::TD_LeftSquareBracket,
@@ -64,76 +52,29 @@ public:
 
 			Clear(olc::VERY_DARK_GREY);
 
-			int nScreenOffsetX_Low = BBC.ram[0x0b91]; 
-			int nScreenOffsetX = BBC.ram[0x0b95];
-			int nScreenOffsetY_Low = BBC.ram[0x0b93]; 
-			int nScreenOffsetY = BBC.ram[0x0b97];
+			int nScreenOffsetX = BBC.ram[0x0b95]; 		int nScreenOffsetY = BBC.ram[0x0b97];
+			int nScreenOffsetX_Low = BBC.ram[0x0b91];	int nScreenOffsetY_Low = BBC.ram[0x0b93];
 
-			int ram_pixels = 0;
-			int ram_bits[8];
-			int pixel_i = 0;
-			int pixel_j = 0;
-			olc::Pixel pixel_col;
+			uint16_t ram_counter = nScreenOffsetX_Low / 4 + (nScreenOffsetX + 104) * 64;
 
-
-			uint16_t ram_counter = 0x6000 + nScreenOffsetX_Low / 4 + (nScreenOffsetX - 152) * 64;
-
-			while (ram_counter < 0x6000) ram_counter += (16 * 16 * 8 * 4);
-			while (ram_counter >= (0x6000 + 16 * 16 * 8 * 4)) ram_counter -= (16 * 16 * 8 * 4);
-
+			//Loop 16x16 character squares, row by row
 			for (int char_j = 0; char_j < 16; char_j++) {
 				for (int char_i = 0; char_i < 16; char_i++) {
 
-					for (int iPixelOffset_i = 0; iPixelOffset_i < 4; iPixelOffset_i++) {
-	 					for (int iPixelOffset_j = 0; iPixelOffset_j < 8; iPixelOffset_j++) {
+					//Loop 8x8 pixels in each character, column by column
+					for (int pixel_i = 0; pixel_i < 4; pixel_i++) {
+	 					for (int pixel_j = 0; pixel_j < 8; pixel_j++) {
 
-							ram_pixels = BBC.ram[ram_counter];
+							int ram_byte = BBC.ram[0x6000 + (ram_counter++ % 0x2000)]; // 0x2000 = size of VRam: 16*8 x 16*8 screen /2 as 2 pixels per byte
+							int ram_bits[8];
+							for (int bit_i = 0; bit_i < 8; bit_i++) {ram_bits[bit_i] = (ram_byte & (1 << bit_i)) != 0;}
 
-							if ((ram_pixels & 0b10000000) == 0b10000000) ram_bits[0] = 1; else ram_bits[0] = 0;
-							if ((ram_pixels & 0b01000000) == 0b01000000) ram_bits[1] = 1; else ram_bits[1] = 0;
-							if ((ram_pixels & 0b00100000) == 0b00100000) ram_bits[2] = 1; else ram_bits[2] = 0;
-							if ((ram_pixels & 0b00010000) == 0b00010000) ram_bits[3] = 1; else ram_bits[3] = 0;
-							if ((ram_pixels & 0b00001000) == 0b00001000) ram_bits[4] = 1; else ram_bits[4] = 0;
-							if ((ram_pixels & 0b00000100) == 0b00000100) ram_bits[5] = 1; else ram_bits[5] = 0;
-							if ((ram_pixels & 0b00000010) == 0b00000010) ram_bits[6] = 1; else ram_bits[6] = 0;
-							if ((ram_pixels & 0b00000001) == 0b00000001) ram_bits[7] = 1; else ram_bits[7] = 0;
-
+							//Each byte contains two interleaved pixels
 							for (int k = 0; k < 2; k++) {
-								//	0	0	0	0	Black
-								//	0	0	1	1	Red
-								//	0	1	0	2	Green
-								//	0	1	1	3	Yellow
-								//	1	0	0	4	Blue
-								//	1	0	1	5	Magenta
-								//	1	1	0	6	Cyan
-								//	1	1	1	7	White
-
-								pixel_col = olc::BLACK;
-								if ((ram_bits[2 + k] == 0) && (ram_bits[4 + k] == 0) && (ram_bits[6 + k] == 0)) pixel_col = olc::BLACK;
-								if ((ram_bits[2 + k] == 0) && (ram_bits[4 + k] == 0) && (ram_bits[6 + k] == 1)) pixel_col = olc::RED;
-								if ((ram_bits[2 + k] == 0) && (ram_bits[4 + k] == 1) && (ram_bits[6 + k] == 0)) pixel_col = olc::GREEN;
-								if ((ram_bits[2 + k] == 0) && (ram_bits[4 + k] == 1) && (ram_bits[6 + k] == 1)) pixel_col = olc::YELLOW;
-								if ((ram_bits[2 + k] == 1) && (ram_bits[4 + k] == 0) && (ram_bits[6 + k] == 0)) pixel_col = olc::BLUE;
-								if ((ram_bits[2 + k] == 1) && (ram_bits[4 + k] == 0) && (ram_bits[6 + k] == 1)) pixel_col = olc::MAGENTA;
-								if ((ram_bits[2 + k] == 1) && (ram_bits[4 + k] == 1) && (ram_bits[6 + k] == 0)) pixel_col = olc::CYAN;
-								if ((ram_bits[2 + k] == 1) && (ram_bits[4 + k] == 1) && (ram_bits[6 + k] == 1)) pixel_col = olc::WHITE;
-
-								pixel_i = char_i * 8 + iPixelOffset_i * 2 + k;
-								pixel_j = char_j * 8 + iPixelOffset_j;
-
-								while (pixel_i < 0) pixel_i += (16 * 8);
-								while (pixel_i >= 16 * 8) pixel_i -= (16 * 8);
-								
-								pixel_j += -nScreenOffsetY_Low / 8 - nScreenOffsetY * 32 - 24;
-								while (pixel_j < 0) pixel_j += (16 * 8);
-								while (pixel_j >= 16 * 8) pixel_j -= (16 * 8);
-
-								Draw(8 + pixel_i, 16 + pixel_j, pixel_col);
+								int screen_i = char_i * 8 + pixel_i * 2 + k;
+								int screen_j = ((char_j + nScreenOffsetY * 12) * 8 + pixel_j + (104 - nScreenOffsetY_Low / 8)) % (16 * 8); // 16 * 8 = height of game screen
+								Draw(8 + screen_i, 16 + screen_j, olc::Pixel(ram_bits[1 - k] * 0xFF, ram_bits[3 - k] * 0xFF, ram_bits[5 - k] * 0xFF));
 							}
-
-							ram_counter++;
-							if (ram_counter == 0x6000 + 16 * 16 * 8 * 4) ram_counter = 0x6000;
-
 						}
 					}
 				}
@@ -149,7 +90,6 @@ int main()
 	Exile_olc6502 exile;
 	//16*8 x 16*8 screen, with 2:1 pixels, plus a border
 	exile.Construct(16 * 8 + 16, 16 * 8 + 32, 6, 3, false, true); 
-
 	exile.Start();
 	return 0;
 }
